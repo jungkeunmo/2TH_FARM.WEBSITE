b06da180de73bb0e0870dec40cb427003ad8c8b4
jungkeunmo <abc59684495@gmail.com>
2022-05-15 22:58:47 +0900

    farm

--- front/components/Header.jsx
+++ front/components/Header.jsx
@@ -56,7 +56,7 @@ const Header = () => {
         <Box>
             <TopBox>
                 <Link href="/">
-                    <CustomLink>HOME</CustomLink>
+                    <CustomLink>COCOFARM</CustomLink>
                 </Link>
             </TopBox>
 
--- front/pages/admin/brandAdmin.jsx
+++ front/pages/admin/brandAdmin.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const BrandAdmin = () => {
+    // 페이지에 진입 할 때 헤더를 비활성화
+
+    // 페이지에서 벗어 날 때 다시 활성화
     return <h2>Brand admin</h2>;
 }
 
